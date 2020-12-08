a15a0d3d40e10ec0a10c3668a7c2cba87cab8d4c
Srammy <zjkhcc@outlook.com>
2020-12-08 10:20:51 +0800

    添加了在剑指offer上的实现

--- JZ6.旋转数组的最小数字.java
+++ JZ6.旋转数组的最小数字.java
@@ -24,3 +24,18 @@ class Solution {
         return numbers[left];
     }
 }
+
+//============================剑指offer=================================
+import java.util.ArrayList;
+public class Solution {
+    public int minNumberInRotateArray(int [] array) {
+        int left = 0, right = array.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (array[mid] > array[right]) left = mid + 1;
+            else if (array[mid] < array[right]) right = mid;
+            else right--;
+        }
+        return array[left];
+    }
+}
